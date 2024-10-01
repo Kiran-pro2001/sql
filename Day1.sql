c2369b7626dacf588550e7e135a414b438ea5b72
Er. Kiran Kumar <kk9840042@gmail.com>
2024-10-01 21:12:23 +0530

    Day 1 - Done

---
+++ Day1.sql
@@ -0,0 +1,41 @@
+SQL By Poojitha Maam 
+Rules - 1. Breakfast / Lunch 
+      - 2. Interaction - Smile (Positive Behaviour/ Positive Vibes)
+      - 3. Be Good to All -- No Backbitch
+
+- No need to worry about the subject. 
+- I will push for the requirements.
+
+No Hurry, Careful crossing the road. 
+
+Fear -- speak speak speak (Then only you will improve).
+
+
+
+
+-- SQL 
+- SQL stands for Structured Query Language 
+
+-- Why we need SQL 
+- SQL is used to communicate with the database.
+
+- There are few operations we can perform on Database
+- We can create the data in the database.
+- We can read the data in the database.
+- We can update the data in the database.
+- We can delete the data from the database.
+
+
+    Data
+______|_____
+|           | ----- CREATE
+|           | ----- READ
+|           | ----- UPDATE
+|___________| ----- DELETE
+  Database
+
+
+
+
+Learning -- Respond to Teacher
+         -- Depth Increase
